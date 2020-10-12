06238444bd9cb861778c84ef4d280f8157f47613
Andrey Popp <8mayday@gmail.com>
2020-10-12 12:37:11 +0300

    docs

--- qc0/scope.py
+++ qc0/scope.py
@@ -1,3 +1,12 @@
+"""
+
+    qc0.scope
+    =========
+
+    This module describes query scoping.
+
+"""
+
 from __future__ import annotations
 
 from typing import Dict, Tuple, Callable, Any, Union, List
@@ -24,34 +33,77 @@ class Scope(Struct):
 
 
 class EmptyScope(Scope):
-    pass
+    """ There's nowhere to navigate from this point."""
 
 
 class UnivScope(Scope):
+    """
+    A universe/initial scope.
+
+    One can navigate to any db tables from this point.
+    """
+
     tables: Dict[str, sa.Table]
 
 
 class TableScope(Scope):
+    """
+    A table scope.
+
+    One can navigate to any of the table columns, fk relationships and reverse
+    fk relationships.
+    """
+
     tables: Dict[str, sa.Table]
     table: sa.Table
 
 
 class RecordScope(Scope):
+    """
+    A scope created by selection.
+    """
+
     scope: Scope
     fields: List[Field]
 
 
 class SyntheticScope(Scope):
+    """
+    Base class for synthetic scopes.
+
+    Such scopes are "synthetic" in a sense that there's no "physical" structure
+    behind them, instead they are being computed by queries.
+    """
+
     def lookup(self, name) -> Tuple[Scope, Callable[Any, Any]]:
+        """
+        Lookup ``name`` in the scope.
+
+        The return value is a tuple of the next scope (it's valid to be
+        ``EmptyScope``) and a function which computes the value in
+        ``sqlalchemy`` terms.
+        """
         raise NotImplementedError()  # pragma: no cover
 
 
 class JsonScope(SyntheticScope):
+    """
+    Scope for JSON values.
+
+    It allows to traverse JSON values using PostgreSQL's ``->`` operator.
+    """
+
     def lookup(self, name):
         return self, lambda v: v[name]
 
 
 class DateScope(SyntheticScope):
+    """
+    Scope for date values.
+
+    It destructures any date into ``year``, ``month`` and ``day`` integers.
+    """
+
     def lookup(self, name):
         names = {
             "year": lambda v: sa.extract("year", v),
